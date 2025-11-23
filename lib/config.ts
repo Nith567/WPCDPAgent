049e8f5f08ae798a0392f44241a186cbe0d1a714
Nith567 <nithinkatkam504106@gmail.com>
2025-11-23 02:48:40 -0300

    cdp auth config

---
+++ lib/config.ts
@@ -0,0 +1,12 @@
+export const CDP_CONFIG = {
+  projectId: process.env.NEXT_PUBLIC_CDP_PROJECT_ID ?? "",
+      ethereum: { // if you want to create an EVM account on login
+          createOnLogin: "smart" // or "smart" for smart accounts
+        },
+         appName: "WPAgent"
+};
+
+export const APP_CONFIG = {
+  name: "WPAgent",
+  logoUrl: "/x402-icon-blue.png",
+};
